fe9f973d65fa7aec03a5901cb880adbe82d4b78f
shiddy <tellhause@gmail.com>
2018-04-19 14:00:01 -0600

    First Commit of final Presentation

---
+++ FinalPrezo/lib/js/html5shiv.js
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
+document.createElement('hgroup');
+||||||| merged common ancestors
+=======
+document.createElement('header');
+document.createElement('nav');
+document.createElement('section');
+document.createElement('article');
+document.createElement('aside');
+document.createElement('footer');
+document.createElement('hgroup');
+
+>>>>>>> said-prez4
